d76c8fea752b069e5453826dec31463f1ce16776
Nhan Nguyen <nhnguyen@mines.edu>
2013-09-30 13:12:39 -0600

    Initial commit for part II

---
+++ src/Merging.java
@@ -0,0 +1,19 @@
+
+public class Merging {
+		public void feedback(int score) {
+			if (score == 100)
+				System.out.println("You're awesome");
+			else if (score > 90)
+				System.out.println("That's great");
+			else if (score > 60)
+				System.out.println("That's good ");
+			else
+				System.out.println("Well, what can I say?");
+		}
+		public static void main(String[] args) {
+			Merging tm = new Merging();
+			tm.feedback(60);
+		}
+	}
+
+}
